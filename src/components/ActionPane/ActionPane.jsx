e9f983d14b62d48d02d26f877d223b4ee50face5
jay <jacob.e.vickers@gmail.com>
2019-02-03 11:05:18 -0700

    create action pane and create global variables

---
+++ src/components/ActionPane/ActionPane.jsx
@@ -0,0 +1,26 @@
+// This is a comment from Logan
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./_action-pane.less";
+
+class ActionPane extends Component {
+  getActionPaneBodyContent = () => {
+    const { headerText } = this.props;
+    return (
+      <div className="action-pane-content-container">
+        <h3 className="action-pane-header">{headerText}</h3>
+        <p>here is some text</p>
+      </div>
+    );
+  };
+
+  render() {
+    return <div className="action-pane">{this.getActionPaneBodyContent()}</div>;
+  }
+}
+
+ActionPane.propTypes = {
+  headerText: PropTypes.string
+};
+
+export default ActionPane;
--- src/components/AtfLayout/AtfLayout.jsx
+++ src/components/AtfLayout/AtfLayout.jsx
@@ -1,19 +1,20 @@
 // This is a comment from Logan
-import React, { Component } from 'react';
-import './_atf-layout.less';
+import React, { Component } from "react";
+import ActionPane from "../ActionPane/ActionPane";
+import "./_atf-layout.less";
 
 class AtfLayout extends Component {
-    render() {
-      return (
-        <div className="atf-layout" >
-          {/* call top pane here, replace hard code with real component */}
-          <div className="top-pane" />
-          {/* call bottom pane here, replace hard code with real component */}
-          <div className="bottom-pane">
-          <div className="action-pane" />
-          </div> 
+  render() {
+    return (
+      <div className="atf-layout">
+        {/* call top pane here, replace hard code with real component */}
+        <div className="top-pane" />
+        {/* call bottom pane here, replace hard code with real component */}
+        <div className="bottom-pane">
+          <ActionPane headerText="Header Here?" />
         </div>
-      );
-    }
+      </div>
+    );
   }
-  export default AtfLayout
+}
+export default AtfLayout;
