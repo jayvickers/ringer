80cdfd15b3b7eb243328b8106ccbada2d0a7a85e
jayvickers <jay@jays-MacBook-Pro.local>
2019-02-03 17:20:54 -0700

    add bottom pane

--- src/components/AtfLayout/AtfLayout.jsx
+++ src/components/AtfLayout/AtfLayout.jsx
@@ -1,6 +1,6 @@
 // This is a comment from Logan
 import React, { Component } from "react";
-import ActionPane from "../ActionPane/ActionPane";
+import BottomPane from "../BottomPane/BottomPane";
 import "./_atf-layout.less";
 
 class AtfLayout extends Component {
@@ -9,10 +9,7 @@ class AtfLayout extends Component {
       <div className="atf-layout">
         {/* call top pane here, replace hard code with real component */}
         <div className="top-pane" />
-        {/* call bottom pane here, replace hard code with real component */}
-        <div className="bottom-pane">
-          <ActionPane headerText="Header Here?" />
-        </div>
+        <BottomPane />
       </div>
     );
   }
---
+++ src/components/BottomPane/BottomPane.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ActionPane from "../ActionPane/ActionPane";
+import "./_bottom-pane.less";
+
+class BottomPane extends Component {
+  render() {
+    return (
+      <div className="bottom-pane">
+        <ActionPane headerText="Header Here?" />
+      </div>
+    );
+  }
+}
+export default BottomPane;
