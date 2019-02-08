879cd2a595f446197ade94df92bfb45e32716e86
jayvickers <jay@jays-MacBook-Pro.local>
2019-02-08 12:08:45 -0700

    implement summoner name input

--- src/components/ActionPane/ActionPane.jsx
+++ src/components/ActionPane/ActionPane.jsx
@@ -7,40 +7,26 @@ class ActionPane extends Component {
     const { headerText } = this.props;
     return (
       <div className="action-pane-content-container">
-        <h1>Floating labels demo</h1>
+        <h3> {headerText}</h3>
+        {this.getSummonerNameInputBox()}
+      </div>
+    );
+  };
 
-        <p>
-          By <a href="https://twitter.com/thatemil">@thatemil</a>
-        </p>
+  getSummonerNameInputBox = () => {
+    return (
+      <div className="summoner-name-container">
         <form action="">
           <div class="field">
             <input
               type="text"
               name="fullname"
               id="fullname"
-              placeholder="Jane Appleseed"
+              placeholder="xMidOrFeed420x"
             />
-            <label for="fullname">Name</label>
-          </div>
-
-          <div class="field">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="jane.appleseed@example.com"
-            />
-            <label for="email">Email</label>
+            <label for="fullname">enter summoner name</label>
           </div>
         </form>
-
-        <p>
-          Will probably work crap in lots of browsers. Works OK in recent
-          Chrome, WebKit/Safari. See the{" "}
-          <a href="http://thatemil.com/blog/2016/01/23/floating-label-no-js-pure-css/">
-            accompanying blog post
-          </a>
-        </p>
       </div>
     );
   };
