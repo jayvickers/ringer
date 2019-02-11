a31934449c74610c08780cd624fab702a74dfc11
jay <jacob.e.vickers@gmail.com>
2019-02-11 15:32:28 -0700

    add second selector

--- src/components/Shared/MultipleChoiceSelect/MultipleChoiceSelect.jsx
+++ src/components/Shared/MultipleChoiceSelect/MultipleChoiceSelect.jsx
@@ -5,19 +5,37 @@ import PropTypes from "prop-types";
 import "./_multiple-choice_select.less";
 
 class MultipleChoiceSelect extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isExpanded: true };
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+    // this.setState({
+    //   isExpanded: !this.state.isExpanded
+    // });
+  };
+
   getRadioButton = (id, label) => {
     return <CustomRadioButton label={label} name={id} id={id} value={id} />;
   };
 
   getRolesPlayed = () => {
-    const top = this.props.lfRoles ? "lf-top" : "top";
-    const mid = this.props.lfRoles ? "lf-mid" : "mid";
-    const jungle = this.props.lfRoles ? "lf-jungle" : "jungle";
-    const bot = this.props.lfRoles ? "lf-bot" : "bot";
-    const support = this.props.lfRoles ? "lf-support" : "support";
+    const { lfRoles } = this.props;
+
+    const top = lfRoles ? "lf-top" : "top";
+    const mid = lfRoles ? "lf-mid" : "mid";
+    const jungle = lfRoles ? "lf-jungle" : "jungle";
+    const bot = lfRoles ? "lf-bot" : "bot";
+    const support = lfRoles ? "lf-support" : "support";
+    const containerClasses = `segmented-control ${
+      this.state.isExpanded ? "expanded" : ""
+    } ${lfRoles ? "lf-roles" : ""}`;
+
     return (
       <form action="#">
-        <div className="segmented-control">
+        <div className={containerClasses}>
           {this.getRadioButton(top, "top")}
           {this.getRadioButton(jungle, "jungle")}
           {this.getRadioButton(mid, "mid")}
@@ -30,12 +48,18 @@ class MultipleChoiceSelect extends Component {
 
   render() {
     const { lfRoles } = this.props;
-    const headerText = !!lfRoles
+    const duoText = this.state.isExpanded
       ? "Select desired duo role(s)"
-      : "Select your role(s)";
+      : "Select desired duo role(s) - click to expand options";
+    const headerText = !!lfRoles ? duoText : "Select your role(s)";
     return (
       <div className="ringer-input-container">
-        <h4 className={lfRoles && "has-sub-header"}> {headerText} </h4>
+        <h4
+          className={lfRoles && "has-sub-header"}
+          onClick={e => this.handleClick(e)}
+        >
+          {headerText}
+        </h4>
         {lfRoles && <h5> *Searching for all roles by default</h5>}
         {this.getRolesPlayed()}
       </div>
