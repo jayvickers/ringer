309e26d1d7012671e62ac7d71829178ca7ca981b
Joshua Wallace <jtsw@rams.colostate.edu>
2019-02-18 16:00:44 -0700

    add info panels

--- src/components/BottomPane/BottomPane.jsx
+++ src/components/BottomPane/BottomPane.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import InfoPanel from "../InfoPanel/InfoPanel";
 import "./_bottom-pane.less";
 
 class BottomPane extends Component {
   render() {
-    return <div className="bottom-pane" />;
+    const bodyText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non elementum turpis. Nam ac porta erat, et fermentum lectus.';
+
+    return (
+      <div className="bottom-pane">
+        <InfoPanel
+          icon='gamepad'
+          headerText='JT'
+          bodyText={bodyText} />
+        <InfoPanel
+          icon='headset'
+          headerText='Jay'
+          bodyText={bodyText} />
+        <InfoPanel
+          icon='trophy'
+          headerText='Logan'
+          bodyText={bodyText} />
+      </div>
+    );
   }
 }
 export default BottomPane;
