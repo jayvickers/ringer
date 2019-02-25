6a57000e2b423c5d61e0af40dc757ddb943dda45
jay <jacob.e.vickers@gmail.com>
2019-02-25 15:48:51 -0700

    create cards

--- src/components/ResultsPane/ResultCard.jsx
+++ src/components/ResultsPane/ResultCard.jsx
@@ -5,13 +5,33 @@ import "./_result-card.less";
 const ResultCard = props => {
   const getRank = () => {
     return (
-      <div className="result-card-rank-container"> {props.summonerName}</div>
+      <div className="result-card-rank-container">
+        {`${props.rank} - ${props.winRate} winrate - 100 games`}
+      </div>
     );
   };
 
   const getResultCardBodyContent = () => {
     return (
-      <div className="result-card-content-container"> {props.summonerName}</div>
+      <div className="result-card-content-container">
+        {getSummonerName()} {getRank()} {getTopChampions()}
+      </div>
+    );
+  };
+
+  const getSummonerName = () => {
+    return (
+      <div className="result-card-name-container"> {props.summonerName}</div>
+    );
+  };
+
+  const getTopChampions = () => {
+    return (
+      <div className="result-card-top-champions-container">
+        <div className="champion-container">
+          Teemo - 25% winrate - 100 games
+        </div>
+      </div>
     );
   };
 
@@ -21,7 +41,9 @@ const ResultCard = props => {
 };
 
 ResultCard.propTypes = {
-  summonerName: PropTypes.string
+  rank: PropTypes.string,
+  summonerName: PropTypes.string,
+  winRate: PropTypes.string
 };
 
 export default ResultCard;
--- src/components/ResultsPane/ResultsPane.jsx
+++ src/components/ResultsPane/ResultsPane.jsx
@@ -6,19 +6,21 @@ import "./_results-pane.less";
 const ResultsPane = props => {
   //probably will iterate through array and return 1 card per match
   //for now can just force 5 cards for templating
-  const getResultCards = summonerName => {
-    return <ResultCard summonerName={summonerName} />;
+  const getResultCards = (rank, summonerName, winRate) => {
+    return (
+      <ResultCard rank={rank} summonerName={summonerName} winRate={winRate} />
+    );
   };
   const getResultsPaneBodyContent = () => {
     return (
       <div className="results-pane-content-container">
         <h3> {"muh results"}</h3>
         <div className="result-card-row">
-          {getResultCards("jtsw")}
-          {getResultCards("xmithie")}
-          {getResultCards("CELLSINTERLINKED")}
-          {getResultCards("doublelift")}
-          {getResultCards("corejj")}
+          {getResultCards("iron 5", "jtsw", "0%")}
+          {getResultCards("challenger", "xmithie", "10%")}
+          {getResultCards("iron 5", "CELLSINTERLINKED", "20%")}
+          {getResultCards("challenger", "doublelift", "30%")}
+          {getResultCards("challenger", "corejj", "40%")}
         </div>
       </div>
     );
