30e73b52dba2d5d5c454b4c4c77272c6a6a9dd09
jayvickers <jay@jays-MacBook-Pro.local>
2019-03-09 13:49:28 -0700

    test data for grid

--- src/components/ResultsPane/ResultGrid.jsx
+++ src/components/ResultsPane/ResultGrid.jsx
@@ -3,43 +3,11 @@ import PropTypes from "prop-types";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham.css";
+import testdata from "./testdata";
 //import classNames from "classnames";
 import "./_result-grid.less";
 const ResultGrid = props => {
   const getGrid = () => {
-    const testdata = {
-      columnDefs: [
-        {
-          headerName: "Make",
-          field: "make"
-        },
-        {
-          headerName: "Model",
-          field: "model"
-        },
-        {
-          headerName: "Price",
-          field: "price"
-        }
-      ],
-      rowData: [
-        {
-          make: "Toyota",
-          model: "Celica",
-          price: 35000
-        },
-        {
-          make: "Ford",
-          model: "Mondeo",
-          price: 32000
-        },
-        {
-          make: "Porsche",
-          model: "Boxter",
-          price: 72000
-        }
-      ]
-    };
     return (
       <div className="">
         <div
